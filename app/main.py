6d77a1181fe16d628ed1e5d75ad62cd598aafc66
esmiraSar <esmirasar@yandex.ru>
2025-10-01 13:45:24 +0400

    update

--- app/main.py
+++ app/main.py
@@ -8,18 +8,8 @@ from api.v1 import router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print("Start")
     app.state.client = Llama(model_path="./model/YandexGPT-5-Lite-8B-instruct-Q4_K_M.gguf", verbose=False)
-    print("End")
     yield
-        #if app.state.client:
-            #del app.state.client
-
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router=router)
-
-@app.get("/test")
-async def tets():
-    return {"message": "success"}
-
